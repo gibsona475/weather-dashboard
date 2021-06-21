117bda201937f02d0648e8e41bed8e88b8a9fd49
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-06-21 09:57:21 -0700

    API Key Added to JS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=820d9bcc76e4697ebc5ef4d9284169ca';
+var apiKey = '820d9bcc76e4697ebc5ef4d9284169ca'
+
 
 fetch(requestUrl)
   .then(function (response) {
